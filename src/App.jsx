2b76d3ae52affc58b16b0a882eba6713f07bf8f1
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-07-20 17:06:56 -0300

    feat: cria App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import ProposalsList from './components/ProposalList.jsx';
 
 const App = () => {
-	return <></>;
+	const [proposals, setProposals] = useState([]);
+
+	useEffect(() => {
+		const fetchProposals = async () => {
+			try {
+				const res = await fetch(
+					'https://jlcambraia.app.n8n.cloud/webhook/propostas'
+				);
+				const data = await res.json();
+				setProposals(data);
+			} catch (error) {
+				console.error('Erro ao buscar propostas:', error);
+			}
+		};
+
+		fetchProposals();
+
+		const intervalId = setInterval(fetchProposals, 10000);
+
+		return () => clearInterval(intervalId);
+	}, []);
+
+	return (
+		<div className='app'>
+			<header className='app__header'>
+				<h1 className='app__title'>Propostas Comerciais</h1>
+			</header>
+
+			<main className='app__content'>
+				<ProposalsList proposals={proposals} />
+			</main>
+		</div>
+	);
 };
 
 export default App;
